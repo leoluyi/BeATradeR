6370651f1cd9f57f71c10dc55df46932b266ce19
leoluyi <leo0650@gmail.com>
2015-11-17 18:57:57 +0800

    install quantstrat on Mac

---
+++ _install quantstrat.R
@@ -0,0 +1,10 @@
+## Install gfortran (for Mac)
+$ curl -O http://r.research.att.com/libs/gfortran-4.8.2-darwin13.tar.bz2
+$ sudo tar fvxz gfortran-4.8.2-darwin13.tar.bz2 -C /
+
+
+install.packages("PerformanceAnalytics")
+install.packages("foreach")
+install.packages("quantstrat", 
+                 repos="http://R-Forge.R-project.org", type ="source")
+install.packages("rvest")
